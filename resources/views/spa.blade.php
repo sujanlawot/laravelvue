5bf13d8970a75f07b4768ab8f64259d258033902
Sujan Lawot <sujan.lawot@gmail.com>
2021-12-12 07:15:14 +0545

    Working on Metronic Dashboard

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,11 +5,115 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"/>        <!--end::Fonts-->
+
+    <!--begin::Page Vendors Styles(used by this page)-->
+    {{-- <link rel="stylesheet" href=""> --}}
+    <link rel="stylesheet" href="">
+            <link href="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+        <!--end::Page Vendors Styles-->
+
+
+<!--begin::Global Theme Styles(used by all pages)-->
+    <link href="{{asset('assets/plugins/global/plugins.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/style.bundle.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<!--end::Global Theme Styles-->
+
+<!--begin::Layout Themes(used by all pages)-->
+
+<link href="{{asset('assets/css/themes/layout/header/base/light.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets/css/themes/layout/header/menu/light.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets/css/themes/layout/brand/dark.css?v=7.0.6')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('assets/css/themes/layout/aside/dark.css?v=7.0.6" rel="stylesheet" type="text/css')}}"/>        <!--end::Layout Themes-->
+
+<link rel="shortcut icon" href="{{asset('assets/media/logos/favicon.ico')}}"/>
+
 </head>
 
-<body>
+<body id="kt_body"  class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading" >
     <div id="app">
     </div>
 </body>
+<script>var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";</script>
+        <!--begin::Global Config(global config for global JS scripts)-->
+        <script>
+            var KTAppSettings = {
+    "breakpoints": {
+        "sm": 576,
+        "md": 768,
+        "lg": 992,
+        "xl": 1200,
+        "xxl": 1400
+    },
+    "colors": {
+        "theme": {
+            "base": {
+                "white": "#ffffff",
+                "primary": "#3699FF",
+                "secondary": "#E5EAEE",
+                "success": "#1BC5BD",
+                "info": "#8950FC",
+                "warning": "#FFA800",
+                "danger": "#F64E60",
+                "light": "#E4E6EF",
+                "dark": "#181C32"
+            },
+            "light": {
+                "white": "#ffffff",
+                "primary": "#E1F0FF",
+                "secondary": "#EBEDF3",
+                "success": "#C9F7F5",
+                "info": "#EEE5FF",
+                "warning": "#FFF4DE",
+                "danger": "#FFE2E5",
+                "light": "#F3F6F9",
+                "dark": "#D6D6E0"
+            },
+            "inverse": {
+                "white": "#ffffff",
+                "primary": "#ffffff",
+                "secondary": "#3F4254",
+                "success": "#ffffff",
+                "info": "#ffffff",
+                "warning": "#ffffff",
+                "danger": "#ffffff",
+                "light": "#464E5F",
+                "dark": "#ffffff"
+            }
+        },
+        "gray": {
+            "gray-100": "#F3F6F9",
+            "gray-200": "#EBEDF3",
+            "gray-300": "#E4E6EF",
+            "gray-400": "#D1D3E0",
+            "gray-500": "#B5B5C3",
+            "gray-600": "#7E8299",
+            "gray-700": "#5E6278",
+            "gray-800": "#3F4254",
+            "gray-900": "#181C32"
+        }
+    },
+    "font-family": "Poppins"
+};
+        </script>
+        <!--end::Global Config-->
+
+    	<!--begin::Global Theme Bundle(used by all pages)-->
+    	    	   <script src="{{asset('assets/plugins/global/plugins.bundle.js?v=7.0.6')}}"></script>
+		    	   <script src="{{asset('assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.6')}}"></script>
+		    	   <script src="{{asset('assets/js/scripts.bundle.js?v=7.0.6')}}"></script>
+				<!--end::Global Theme Bundle-->
+
+                    <!--begin::Page Vendors(used by this page)-->
+                            <script src="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js?v=7.0.6')}}"></script>
+                        <!--end::Page Vendors-->
+
+                    <!--begin::Page Scripts(used by this page)-->
+                            <script src="{{asset('assets/js/pages/widgets.js?v=7.0.6')}}"></script>
+                        <!--end::Page Scripts-->
+
 <script src="{{ asset('js/app.js') }}"></script>
+
 </html>
